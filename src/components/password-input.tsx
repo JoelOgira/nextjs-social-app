9559606a8a0beeca33c8e364d1e949a1417131b1
Jay <joelogira@gmail.com>
2024-08-03 01:03:36 +0300

    Created password input component

---
+++ src/components/password-input.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+
+import { cn } from "@/lib/utils";
+import { Input, InputProps } from "./ui/input";
+import { Eye, EyeOff } from "lucide-react";
+
+const PasswordInput = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    const [showPassword, setShowPassword] = React.useState<boolean>(false);
+
+    return (
+      <div className="relative">
+        <Input
+          type={showPassword ? "text" : "password"}
+          className={cn("pe-10", className)}
+          ref={ref}
+          {...props}
+        />
+        <button
+          type="button"
+          className="absolute right-3 top-1/2 -translate-y-1/2 text-muted-foreground"
+          onClick={() => setShowPassword(!showPassword)}
+          title={showPassword ? "Hide Password" : "Show Password"}
+        >
+          {showPassword ? (
+            <EyeOff className="size-5" />
+          ) : (
+            <Eye className="size-5" />
+          )}
+        </button>
+      </div>
+    );
+  },
+);
+PasswordInput.displayName = "PasswordInput";
+
+export { PasswordInput };
