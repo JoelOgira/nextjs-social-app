297eff6ac9c711518ce0d4e9237bd9996735594b
Jay <joelogira@gmail.com>
2024-08-05 22:08:44 +0300

    Created timeline page

---
+++ src/app/(timeline)/page.tsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+
+export default function Timeline() {
+  return (
+    <main className="flex h-full justify-center font-sans">
+      <MaxWidthWrapper>
+        Welcome to your timeline
+      </MaxWidthWrapper>
+    </main>
+  );
+}
