9e4333d9ee787bf62eb49d233fda941fb5b0fdc4
Jay <joelogira@gmail.com>
2024-08-07 21:31:57 +0300

    Modified styling and added menu bar routes

--- src/app/(timeline)/layout.tsx
+++ src/app/(timeline)/layout.tsx
@@ -2,6 +2,8 @@ import { validateRequest } from "@/auth";
 import { redirect } from "next/navigation";
 import SessionProvider from "./SessionProvider";
 import Navbar from "../../components/navbar";
+import MaxWidthWrapper from "@/components/max-width-wrapper";
+import MenuBar from "./menu-bar";
 
 export default async function AuthLayout({
   children,
@@ -15,7 +17,11 @@ export default async function AuthLayout({
   return (
     <SessionProvider value={session}>
       <Navbar />
-      {children}
+      <MaxWidthWrapper className="flex grow gap-5 pt-5">
+        <MenuBar className="sticky hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-1 py-5 shadow-sm sm:block lg:px-3 xl:w-80" />
+        {children}
+      </MaxWidthWrapper>
+      <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-1 sm:hidden" />
     </SessionProvider>
   );
 }
